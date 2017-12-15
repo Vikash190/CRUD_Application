8ae693f4ecb80a54948d0a523627a49cedaab5b5
Vikash <singhvikash611@gmail.com>
2017-12-15 13:43:12 +0530

    Update server.js
    added the front page to the application

--- server.js
+++ server.js
@@ -1,28 +1,40 @@
 const express =require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
+let db;
 
-//body parser to be added before all the functions
-//to ensure the use of input boxes getting input from user
-//urlencoded ensures bodyparser to extract data from the form
-//./node_modules/.bin/nodemon server.js for opening  server single time
-app.use(bodyParser.urlencoded({extended:true}))
-//using this middleware to tell express to make public folder accessible
-app.use(express.static('public'))
-app.use(bodyParser.json())
-app.set('view engine','ejs')
-var db
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.set('view engine','ejs');
 
 MongoClient.connect('mongodb://vikash:vikash123@ds036079.mlab.com:36079/vikashdb', function(err, database)  {
-    // ... start the server
-    if(err)return console.log(err)
-    db=database
-//operation yet to provide
+  
+    if(err)return console.log(err);
+    db=database;
     app.listen(3000, function () {
         console.log('listening on 3000');
     })
 })
+
+
+app.get("/" , function (req,res){
+
+        res.sendFile(__dirname + "/index.html")
+})
+
+app.get('/getQuotes', function(request, response)  {
+    //  response.sendFile('C:\\Users\\admin\\Documents\\web1' + '/index1.html');
+    //var cursor =db.collection('quotes').find();
+    db.collection('quotes').find().toArray(function(err, result){
+        if (err) return console.log(err)
+        // renders index.ejs
+        response.render('index.ejs', {quotes: result})
+
+    })
+});
+
 
 app.post('/quotes', function(req, res)  {
     db.collection('quotes').save(req.body, function(err, result)  {
@@ -34,19 +46,12 @@ app.post('/quotes', function(req, res)  {
 })
 //getting the records from the mongodb
 
-app.get('/', function(request, response)  {
-  //  response.sendFile('C:\\Users\\admin\\Documents\\web1' + '/index1.html');
-    //var cursor =db.collection('quotes').find();
-    db.collection('quotes').find().toArray(function(err, result){
-        if (err) return console.log(err)
-        // renders index.ejs
-        response.render('index.ejs', {quotes: result})
-})
-})
+
+
 app.put('/quotes', function(req, res)  {
     // Handle put request
     db.collection('quotes')
-        .findOneAndUpdate({name: 'Abraham Maslow'}, {
+        .findOneAndUpdate({name: Name}, {
             $set: {
                 name: req.body.name,
                 quote: req.body.quote
@@ -55,14 +60,14 @@ app.put('/quotes', function(req, res)  {
             sort: {_id: -1},
             upsert: true
         }, function(err, result) {
-        if (err) return res.send(err)
-        res.send(result)
-})
+            if (err) return res.send(err)
+            res.send(result)
+        })
 })
 app.delete('/quotes', function(req, res)  {
     db.collection('quotes').findOneAndDelete({name: req.body.name},
-    function(err, result)  {
-    if (err) return res.send(500, err)
-    res.send({message: ' Baltasar Gracian quote got deleted'})
-})
+        function(err, result)  {
+            if (err) return res.send(500, err)
+            res.send({message: ' Baltasar Gracian quote got deleted'})
+        })
 })
